77aa90fcfd7adf0acd1846cfad129fce475c08c1
Benjamin Jackson <bhjackson@gmail.com>
2011-11-24 22:02:17 -0500

    Added support for users api which allows clients to get the current authenticated user as well as an arbitrary user record by name.

---
+++ spec/user_api_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe GitHubV3API::UsersAPI do
+  describe '#current' do
+    it 'returns the user data for the authenticated user' do
+      connection = mock(GitHubV3API)
+      connection.should_receive(:get).with('/user').and_return(:user_hash1)
+      api = GitHubV3API::UsersAPI.new(connection)
+      GitHubV3API::User.should_receive(:new).with(api, :user_hash1).and_return(:user1)
+      user = api.current
+      user.should == :user1
+    end
+  end
+
+  describe '#get' do
+    it 'returns a fully-hydrated User object for the specified user login' do
+      connection = mock(GitHubV3API)
+      connection.should_receive(:get).with('/users/octocat').and_return(:user_hash)
+      api = GitHubV3API::UsersAPI.new(connection)
+      GitHubV3API::User.should_receive(:new_with_all_data).with(api, :user_hash).and_return(:user)
+      api.get('octocat').should == :user
+    end
+  end
+
+end
---
+++ spec/user_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+
+describe GitHubV3API::User do
+  describe 'attr_readers' do
+    it 'should define attr_readers that pull values from the user data' do
+      fields = %w(login id avatar_url gravatar_id url name company blog location 
+                  email hireable bio public_repos public_gists followers following 
+                  html_url created_at type total_private_repos owned_private_repos 
+                  private_gists disk_usage collaborators)
+      fields.each do |f|
+        user = GitHubV3API::User.new_with_all_data(stub('api'), {f.to_s => 'foo'})
+        user.methods.should include(f.to_sym)
+        user.send(f).should == 'foo'
+      end
+    end
+  end
+
+  describe '#[]' do
+    it 'returns the user data for the specified key' do
+      api = mock(GitHubV3API::UsersAPI)
+      api.should_receive(:get).with('github') \
+        .and_return(GitHubV3API::User.new(api, 'login' => 'github', 'name' => 'GitHub'))
+      user = GitHubV3API::User.new(api, 'login' => 'github')
+      user['name'].should == 'GitHub'
+    end
+
+    it 'only fetches the data once' do
+      api = mock(GitHubV3API::UsersAPI)
+      api.should_receive(:get).once.with('github') \
+        .and_return(GitHubV3API::User.new(api, 'login' => 'github', 'name' => 'GitHub'))
+      user = GitHubV3API::User.new(api, 'login' => 'github')
+      user['login'].should == 'github'
+      user['name'].should == 'GitHub'
+      user['foo'].should be_nil
+    end
+  end
+
+end
